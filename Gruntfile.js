9df987c5abc738b5adb34f91b78b26bac003ff84
Anna Vasilyeva <vasileva.ann@gmail.com>
2015-03-17 21:38:34 +0300

    Стилизация мобильной версии проекта с использованием LESS.

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,7 +16,7 @@ module.exports = function(grunt) {
           newline: true,
           newlineMaximum: 2,
           trailingspaces: true,
-          indentationGuess: true,
+          indentationGuess: false,
           editorconfig: '.editorconfig',
           ignores: [
             'html-comments',
@@ -26,10 +26,61 @@ module.exports = function(grunt) {
           showCodes: true
         }
       }
-    }
+    },
+
+    less: {
+      style: {
+        files: {
+          'css/style.css': ['less/style.less']
+        }
+      }
+    },
+
+    watch: {
+      options: {
+        livereload: true,
+        spawn:false
+      },
+      style: {
+        files: ['less/**/*.less'],
+        tasks: ['less'],
+      },
+      html: {
+        files: ['*.html'],
+      }
+    },
+
+    postcss: {
+      options: {
+        processors: [
+          require('autoprefixer-core')({ browsers: ['last 2 version'] }).postcss
+        ]
+      },
+      dist: { src: 'css/style.css' }
+    },
+
+    csswring: {
+      options: {
+        map: true
+      },
+      main: {
+        dest: 'css/style.min.css',
+        src: ['css/style.css']
+      }
+    },
+
   });
 
   grunt.loadNpmTasks('grunt-lintspaces');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-postcss');
+  grunt.loadNpmTasks('csswring');
 
   grunt.registerTask('lint', ['lintspaces']);
+  grunt.registerTask('build:development', [ 'less', 'postcss' ]);
+  grunt.registerTask('build', [ 'build:development' ]);
+  grunt.registerTask('default', [ 'build:development', 'watch' ]);
+  grunt.registerTask('build:production', [ 'less', 'postcss', 'csswring' ]);
+
 };
